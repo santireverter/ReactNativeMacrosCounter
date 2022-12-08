cdec9308f17c377127f8c5775bc89e197699f4f6
santiago reverter <sreverter@outlook.com>
2022-12-08 19:15:48 -0300

    [feature] modificacion de los iconos de los bottomtabs

--- src/navigation/tabs.jsx
+++ src/navigation/tabs.jsx
@@ -1,15 +1,34 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import AppNavigation from "./appNavigation";
 import ComidasCargadasNavigation from "./comidasCargadas";
+import { Ionicons } from '@expo/vector-icons';
+import colors from "../constants/colors";
 
 const BottomTab = createBottomTabNavigator();
 
 const Tabs = () =>{
     return (
         <BottomTab.Navigator initialRouteName="CounterTab" screenOptions={{headerShown: false,}}>
-            <BottomTab.Screen name="CounterTab" component={AppNavigation} />
-            <BottomTab.Screen name="ComidasTab" component={ComidasCargadasNavigation} />
+            <BottomTab.Screen 
+                name="CounterTab"
+                component={AppNavigation}
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name={ focused ? 'home' : 'home-outline'} size={22}  color={colors.borderColor}/>
+                    )
+                }}/>
+            <BottomTab.Screen 
+                name="ComidasTab"
+                component={ComidasCargadasNavigation}
+                options={{
+                    title: 'Comidas',
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name={ focused ? 'fast-food' : 'fast-food-outline'} size={22}  color={colors.borderColor}/>
+                    )
+                }}/>
         </BottomTab.Navigator>
     );
 };
